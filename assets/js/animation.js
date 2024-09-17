32d9df51fe8fa0c62ba71e65df26da418cbcaab9
Lunessel <varloky@gmail.com>
2024-09-17 09:21:12 +0300

    add some animations

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,7 +1,13 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".first-screen h2", { x: 0, duration: .5 });
-gsap.to(".first-screen .img-container", { opacity: 1, x: 0, duration: .5 });
+let mm = gsap.matchMedia();
+
+gsap.to(".first-screen h2", {opacity: 1, x: 0, duration: .6 });
+
+// desktop anim
+mm.add('(min-width: 878px)', () => {
+    gsap.to(".first-screen .img-container", { opacity: 1, x: 0, duration: .5 });
+})
 gsap.to(".feedbacks .slider", 
     {
         opacity: 1,
@@ -9,25 +15,36 @@ gsap.to(".feedbacks .slider",
         duration: .5,
         scrollTrigger: {
             trigger: ".feedbacks .slider",
-            start: "top 90%",  // Start when element hits 90% of viewport height
-            end: "top 30%",    // End when element hits 30% of viewport height
-            scrub: true,       // Enables smooth scroll-based animation
-            toggleActions: "play pause resume reset" // Animation control on scroll
+            start: "top 90%",
+            end: "top 30%",
+            scrub: true,
+            toggleActions: "play pause resume reset" 
         }
 
     });
 
-gsap.to(".articles", 
-    {
-        opacity: 1,
-        y: 0,
-        duration: .5,
-        scrollTrigger: {
-            trigger: ".articles",
-            start: "top 90%",  // Start when element hits 90% of viewport height
-            end: "top 30%",    // End when element hits 30% of viewport height
-            scrub: true,       // Enables smooth scroll-based animation
-            toggleActions: "play pause resume reset" // Animation control on scroll
-        }
+const articles = document.querySelectorAll(".article");
+articles.forEach((article, index) => {
+    const animation = gsap.fromTo(article, 
+        {
+            opacity: 0,
+            transform: 'translateY(10svh)'
+        },
+        {
+            opacity: 1,
+            transform: 'translateY(0px)',
+            ease: "power1.inOut",
+            delay: (index + 1) * 0.2,
 
-    });
+            scrollTrigger: {
+                trigger: article,
+                once: false,
+                start: 'top, bottom 100%',
+                toggleActions: 'play none none reverse',
+                onEnter: () => animation.restart(true),
+                onLeaveBack: () => animation.reverse(),
+                invalidateOnRefresh: true
+            }
+    
+        });
+})
